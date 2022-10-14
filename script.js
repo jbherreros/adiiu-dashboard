370ff0e561a13236070a10fd2f24030792c198cf
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2022-10-14 10:21:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,124 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/spain?unitGroup=metric&key=GJ6DLKPEWZBH4LD8TUYMAVL4L&contentType=json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log(data.days[0].temp);
+    });
+
+
+
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+
+    const chart2 = Highcharts.chart('container2', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+
+// Data retrieved from https://netmarketshare.com
+    const chart3 = Highcharts.chart('container3', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Browser market shares in May, 2020'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+      point: {
+        valueSuffix: '%'
+      }
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+      }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      data: [{
+        name: 'Chrome',
+        y: 70.67,
+        sliced: true,
+        selected: true
+      }, {
+        name: 'Edge',
+        y: 14.77
+      },  {
+        name: 'Firefox',
+        y: 4.86
+      }, {
+        name: 'Safari',
+        y: 2.63
+      }, {
+        name: 'Internet Explorer',
+        y: 1.53
+      },  {
+        name: 'Opera',
+        y: 1.40
+      }, {
+        name: 'Sogou Explorer',
+        y: 0.84
+      }, {
+        name: 'QQ',
+        y: 0.51
+      }, {
+        name: 'Other',
+        y: 2.6
+      }]
+    }]
+  });
+
+});
